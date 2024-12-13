f032ce518f6847fa3c9912a58d53f903833221a2
sungjae <dtc13579@gmail.com>
2024-12-13 15:33:46 +0900

    feat: 점프 투 스프링부트 강의
    - 2-04. 엔티티로 테이블 매핑하기
    - @Entity에서 사용할 속성 엔티티 학습

---
+++ jsb/src/main/java/com/mysite/jsb/Question.java
@@ -0,0 +1,34 @@
+package com.mysite.jsb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Question {
+
+    /* [strategy 옵션 생략 시]
+     * @GeneratedValue가 지정된 모든 속성에 번호를 차례대로 생성하므로 고유 번호를 가질 수 없음.
+     * */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    /*[CascadeType.REMOVE]
+    * 질문을 삭제하면 그에 달린 답변들도 삭제 (연쇄 삭제 하기 위해)*/
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+}
