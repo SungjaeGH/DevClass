b7776b9262039d8dddd3b3e88f9b64d784df784c
sungjae <dtc13579@gmail.com>
2024-12-17 15:09:35 +0900

    feat: 점프 투 스프링부트 강의
    - 2-16. 질문 등록 기능 추가하기
    - 질문 등록 기능 추가 및 항목 유효성 체크를 위한 validation 의존성 추가

--- jsb/src/main/java/com/mysite/jsb/question/QuestionService.java
+++ jsb/src/main/java/com/mysite/jsb/question/QuestionService.java
@@ -3,6 +3,7 @@ package com.mysite.jsb.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -18,5 +19,15 @@ public class QuestionService {
     public Question getQuestion(Integer id) {
         return questionRepository.findById(id)
                 .orElseThrow(() -> new DataNotFoundException("Question not found"));
+    }
+
+    public void create(String subject, String content) {
+
+        Question question = new Question();
+        question.setSubject(subject);
+        question.setContent(content);
+        question.setCreateDate(LocalDateTime.now());
+
+        questionRepository.save(question);
     }
 }
