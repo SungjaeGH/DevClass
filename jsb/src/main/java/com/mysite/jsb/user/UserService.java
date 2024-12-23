4e73649e6a3781324497656ac85bd0a6e88b0671
sungjae <dtc13579@gmail.com>
2024-12-23 16:29:47 +0900

    feat: 점프 투 스프링부트 강의
    - 3-06. 회원 가입 기능 구현하기
    - 회원 가입을 위한 View, Entity, Controller, Service, Repository 추가

---
+++ jsb/src/main/java/com/mysite/jsb/user/UserService.java
@@ -0,0 +1,24 @@
+package com.mysite.jsb.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public SiteUser create(String username, String email, String password) {
+
+        SiteUser siteUser = new SiteUser();
+        siteUser.setUsername(username);
+        siteUser.setEmail(email);
+        siteUser.setPassword(passwordEncoder.encode(password));
+
+        return userRepository.save(siteUser);
+    }
+}
