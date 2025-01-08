ee500dede3c075b0bd5a9b4c89a7d0fda92b4ad0
sungjae <dtc13579@gmail.com>
2025-01-08 13:22:08 +0900

    feat: 프로그래머스 이재환의 자바프로그래밍 입문 에제 문제 추가
    - Part 16, 17 예제 문제 추가

---
+++ 이재환의 자바프로그래밍 입문/자바의 기본 클래스/Object 클래스 복습.java
@@ -0,0 +1,8 @@
+/*
+ * 다음 중 옳지 않은 것을 고르세요. (4)
+1. java.lang 패키지에 속한 클래스들은 임포트하지 않아도 자동으로 임포트된다.
+2. 모든 자바 클래스는 Object 클래스로부터 상속을 받는다.
+3. Object 클래스는 java.lang 패키지에 속해 있다.
+4. ArithmeticException 클래스의 최상위 클래스는 Throwable 클래스이다.
+5. Object 클래스의 메서드에는 toString(), equals(), hashcode(), clone() 등이 있다.
+* */
