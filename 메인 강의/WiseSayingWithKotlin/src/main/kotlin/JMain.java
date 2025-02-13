ddd17f901f7a95e9450e1103d002a26d604d4914
sungjae <dtc13579@gmail.com>
2025-02-13 14:40:14 +0900

    feat: 코틀린 기본 세팅
    - 코틀린과 자바 비교 코드 출력

---
+++ 메인 강의/WiseSayingWithKotlin/src/main/kotlin/JMain.java
@@ -0,0 +1,9 @@
+public class JMain {
+    public static void main(String[] args) {
+        final int number = 10;
+        String message = "Hello Java!";
+
+        System.out.println("number = " + number);
+        System.out.println("message = " + message);
+    }
+}
---
+++ 메인 강의/WiseSayingWithKotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+package com.ll
+
+fun main() {
+    val number = 10
+    var message = "Hello Kotlin!"
+
+    println("number is $number")
+    println("message is $message")
+}
